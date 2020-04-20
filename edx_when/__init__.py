b2103cc6286db54f20b5ec4aaf60bd8851528556
Soban Javed <iamsobanjaved@gmail.com>
2020-04-20 12:30:51 +0500

    added python 3.8 in test matrix

--- edx_when/__init__.py
+++ edx_when/__init__.py
@@ -4,6 +4,6 @@ Your project description goes here.
 
 from __future__ import absolute_import, unicode_literals
 
-__version__ = '1.2.2'
+__version__ = '1.2.3'
 
 default_app_config = 'edx_when.apps.EdxWhenConfig'  # pylint: disable=invalid-name
