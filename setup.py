820c66aae315cbd742c8c037f84c098e9e0cbbfa
Sarina Canelake <sarina@tcril.org>
2022-09-10 16:13:51 -0400

    fix: fix github url strings (org edx -> openedx)

--- setup.py
+++ setup.py
@@ -111,7 +111,7 @@ setup(
     long_description_content_type='text/x-rst',
     author='edX',
     author_email='oscm@edx.org',
-    url='https://github.com/edx/edx-when',
+    url='https://github.com/openedx/edx-when',
     packages=[
         'edx_when',
     ],
