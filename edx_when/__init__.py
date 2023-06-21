30456176dc131039f8c6e9a59e07fb33b2afeff6
Salman Nawaz <salman.nawaz@A006-01081.local>
2023-06-21 13:53:21 +0500

    fix: update for django 4.2

--- edx_when/__init__.py
+++ edx_when/__init__.py
@@ -4,4 +4,3 @@ Central source of course block dates for the LMS.
 
 __version__ = '2.3.0'
 
-default_app_config = 'edx_when.apps.EdxWhenConfig'  # pylint: disable=invalid-name
--- edx_when/urls.py
+++ edx_when/urls.py
@@ -3,14 +3,14 @@ URLs for edx_when.
 """
 
 from django.conf import settings
-from django.conf.urls import url
+from django.urls import re_path
 
 from . import views
 
 app_name = 'edx_when'
 
 urlpatterns = [
-    url(
+    re_path(
         r'edx_when/course/{}'.format(settings.COURSE_ID_PATTERN),
         views.CourseDates.as_view(),
         name='course_dates'
