9d0de4d437ea93912c736f3ca9d3be5207714d0c
Andrii <andrii.hantkovskyi@raccoongang.com>
2025-07-07 12:47:57 +0300

    refactor: add processing for all enrolled courses if no course_id provided

--- edx_when/rest_api/v1/views.py
+++ edx_when/rest_api/v1/views.py
@@ -23,14 +23,16 @@ class UserDatesView(APIView):
         """
         **Use Cases**
 
-            Request user dates for a specific course.
+            Request user dates for a specific course or all enrolled courses.
 
         **Example Requests**
 
+            GET /api/edx_when/v1/user-dates/
             GET /api/edx_when/v1/user-dates/{course_id}
 
         **Parameters:**
 
+            course_id: (optional, str) Course ID to get dates for. If not provided, returns dates for all enrolled courses.
             block_types: (optional, str) Comma-separated list of block types to filter the dates.
             block_keys: (optional, str) Comma-separated list of block keys to filter the dates.
             date_types: (optional, str) Comma-separated list of date types to filter the dates.
@@ -39,8 +41,15 @@ class UserDatesView(APIView):
 
             Body consists of the following fields:
 
-            * user_dates: (dict) A dictionary containing user-specific dates for the course.
+            * user_dates: (dict) A dictionary containing user-specific dates for the course(s).
                 * The keys are date identifiers and the values are the corresponding date values.
+
+        **Example Response**
+
+            {
+                "block1_due": "2023-10-01T12:00:00Z",
+                "block2_start": "2023-10-05T08:00:00
+            }
 
         **Returns**
 
@@ -54,11 +63,22 @@ class UserDatesView(APIView):
         block_keys = request.query_params.get('block_keys', '').split(',')
         date_types = request.query_params.get('date_types', '').split(',')
 
-        user_dates = get_user_dates(
-            course_id,
-            request.user.id,
-            block_types=block_types if block_types != [''] else None,
-            block_keys=block_keys if block_keys != [''] else None,
-            date_types=date_types if date_types != [''] else None
+        enrolled_courses = (
+            [course_id]
+            if course_id
+            else request.user.courseenrollment_set.filter(is_active=True).values_list(
+                "course_id", flat=True
+            )
         )
-        return Response({str(key[0]): value for key, value in user_dates.items()})
+        all_user_dates = {}
+
+        for enrolled_course_id in enrolled_courses:
+            user_dates = get_user_dates(
+                enrolled_course_id,
+                request.user.id,
+                block_types=block_types if block_types != [''] else None,
+                block_keys=block_keys if block_keys != [''] else None,
+                date_types=date_types if date_types != [''] else None
+            )
+            all_user_dates.update({str(key[0]): value for key, value in user_dates.items()})
+        return Response(all_user_dates)
